26d3de682ad6f4a2dc8025917337bf0e38e63730
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-07-27 20:24:20 +0530

    Fix bind logic in server.cpp

--- server.cpp
+++ server.cpp
@@ -1,28 +1,31 @@
 #include "functions.cpp"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+
 
 int main() {
 
   int serverSocketFD = createTCPSocket();
   struct sockaddr_in serveraddress = createIPv4Address("", 2000);
+
   int result = bind(serverSocketFD, (struct sockaddr *)&serveraddress, sizeof(serveraddress));
-  if (result == 0) {
-    std::cout << "Server is connected\n";
+  if (result == 0)  std::cout << "[+] Server is bound to port 2000\n";
+  else {
+    std::cout << "[-] Bind failed\n";
     return -1;
   }
 
-  int listenResult = listen(serverSocketFD, 10); 
+  listen(serverSocketFD, 10);
+  std::cout << "[*] Waiting for connections...\n";
+  struct AcceptedClients* clientNode = acceptNewConnection(serverSocketFD);
+  if (!clientNode->acceptedSuccessfully) return -1;
 
-  struct sockaddr_in clientAddress;
-  __socklen_t clientAddressSize = sizeof(clientAddress);
-  int clientSocketFD = accept(serverSocketFD, (struct sockaddr *)&clientAddress, &clientAddressSize);
+  receiveAndPrintIncomingData(clientNode);
 
-  char buffer[4098];
-  while (true) {
-    std::memset(buffer, 0, sizeof(buffer));
-    ssize_t data_Recived = recv(clientSocketFD, buffer, 4096, 0);
-
-    if (data_Recived > 0) std::cout << buffer << std::endl;
-     else  break;
-  }
+  close(clientNode->acceptedSocketsFD);
+  delete clientNode; 
+  shutdown(serverSocketFD, SHUT_RDWR);
   return 0;
 }
