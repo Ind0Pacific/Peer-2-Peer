a266c4a9a706cf5ed45084179e9948bb14150d86
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-07-27 20:26:02 +0530

    Add client acceptance and data receiving logic

--- functions.cpp
+++ functions.cpp
@@ -1,33 +1,72 @@
-#include <arpa/inet.h> 
-#include <cstring>      
-#include <iostream>     
-#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <cstring>
 #include <iostream>
-#include <sys/socket.h> 
-#include <unistd.h> 
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+struct AcceptedClients{
+  int acceptedSocketsFD;
+  struct sockaddr_in address;
+  int error;
+  bool acceptedSuccessfully;
+};
 
 int createTCPSocket() {
-    int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (fd < 0) {
-        std::cerr << "[-] Socket creation failed\n";
-        return -1; 
-    }
-    std::cout << "[+] Socket created successfully\n";
-    return fd; 
+   int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+   if (fd < 0) {
+    std::cerr << "[-] Socket creation failed\n";
+    return -1;
   }
+   std::cout << "[+] Socket created successfully\n";
+   return fd;
+}
 
-struct sockaddr_in createIPv4Address(const char* ip, int port) {
-    struct sockaddr_in address;
-    std::memset(&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_port = htons(port);
-    address.sin_addr.s_addr;
-    
-    if(strlen(ip) == 0){
-        address.sin_addr.s_addr = INADDR_ANY;
-        return address;
-    } else  {
-    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
-    return address; 
+struct sockaddr_in createIPv4Address(const char *ip, int port) {
+   struct sockaddr_in address;
+   std::memset(&address, 0, sizeof(address));
+   address.sin_family = AF_INET;
+   address.sin_port = htons(port);
+   address.sin_addr.s_addr;
+
+   if (strlen(ip) == 0) {
+    address.sin_addr.s_addr = INADDR_ANY;
+    return address;
+ } else {
+   inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+   return address;
+  }
+}
+
+struct AcceptedClients* acceptNewConnection(int serverSocketFD) {
+
+   struct AcceptedClients* client = new AcceptedClients;
+  
+   socklen_t clientAddressSize = sizeof(client->address);
+   client->acceptedSocketsFD = accept(serverSocketFD, (struct sockaddr *)&client->address, &clientAddressSize);
+
+   if (client->acceptedSocketsFD < 0) {
+    std::cout << "[-] Failed to accept client connection\n";
+    client->error = client->acceptedSocketsFD;
+    client->acceptedSuccessfully = false;
+  } else {
+    std::cout << "[+] Client connected successfully!\n";
+    client->error = 0;
+    client->acceptedSuccessfully = true;
+  }
+   return client;
+}
+
+void receiveAndPrintIncomingData(struct AcceptedClients *&clientNode) {
+  char buffer[4098];
+  while (true) {
+    std::memset(buffer, 0, sizeof(buffer));
+    ssize_t data_Recived = recv(clientNode->acceptedSocketsFD, buffer, 4096, 0);
+    if (data_Recived > 0)
+      std::cout << buffer << std::endl;
+    else {
+      std::cout << "User has exited the chat";
+      break;
     }
   }
+}
