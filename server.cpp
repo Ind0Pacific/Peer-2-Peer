298a7a69db82402eb34fea401f6f0d7c6eef7db3
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-07-30 21:41:11 +0530

    Reformated code style

--- server.cpp
+++ server.cpp
@@ -1,35 +1,32 @@
 #include "functions.cpp"
+#include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <unistd.h>
-#include <iostream>
 #include <thread>
+#include <unistd.h>
 
 int main() {
 
   int serverSocketFD = createTCPSocket();
   struct sockaddr_in serveraddress = createIPv4Address("", 2000);
-
   int result = bind(serverSocketFD, (struct sockaddr *)&serveraddress, sizeof(serveraddress));
-  if (result == 0)  std::cout << "[+] Server is bound to port 2000\n";
+  if (result == 0)
+    std::cout << "[+] Server is bound to port 2000\n";
   else {
     std::cout << "[-] Bind failed\n";
     return -1;
   }
-
   listen(serverSocketFD, 10);
   std::cout << "[*] Waiting for connections...\n";
   while (true) {
-    struct AcceptedClients* clientNode = acceptNewConnection(serverSocketFD);
+    struct AcceptedClients *clientNode = acceptNewConnection(serverSocketFD);
     if (!clientNode->acceptedSuccessfully) {
-        delete clientNode;
-        continue; 
+      delete clientNode;
+      continue;
     }
     std::thread clientThread(receiveAndPrintIncomingData, clientNode);
-    clientThread.detach(); 
-}
+    clientThread.detach();
+  }
   shutdown(serverSocketFD, SHUT_RDWR);
-
-   
   return 0;
 }
